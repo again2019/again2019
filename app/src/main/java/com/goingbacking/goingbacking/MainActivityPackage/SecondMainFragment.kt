c2e5ff899706804413f1973972448b6968acce7c
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-09-23 17:14:43 +0900

    colum chart 구현 중
    colum chart 구현 중

--- app/src/main/java/com/goingbacking/goingbacking/MainActivityPackage/SecondMainFragment.kt
+++ app/src/main/java/com/goingbacking/goingbacking/MainActivityPackage/SecondMainFragment.kt
@@ -5,32 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.aachartmodel.aainfographics.aachartcreator.*
 import com.goingbacking.goingbacking.R
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
-import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
-import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.fragment_second_main.*
 import kotlinx.android.synthetic.main.fragment_second_main.view.*
 
-class SecondMainFragment : Fragment() {
+class SecondMainFragment : Fragment(), AAChartView.AAChartViewCallBack {
+
+    var aaChartModel = AAChartModel()
+    var aaChartView: AAChartView? = null
+    var chartType: String = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_second_main, container, false)
 
-        // sample chartView 구현 완료
-        val aaChartModel : AAChartModel = AAChartModel()
-            .chartType(AAChartType.Bar)
-            .title("title")
-            .subtitle("subtitle")
-            .dataLabelsEnabled(true)
-            .series(arrayOf(
+        aaChartView = view.AAChartView
+        aaChartView?.setBackgroundColor(0)
+        aaChartView?.callBack = this
+        aaChartModel = configureAAChartModel()
+        aaChartView?.aa_drawChartWithChartModel(aaChartModel)
+
+
+        return view
+    }
+
+
+
+    private fun configureAAChartModel(): AAChartModel {
+        chartType = AAChartType.Column.value
+        val chartTypeEnum = AAChartType.Column
+
+        val aaChartModel = AAChartModel.Builder(requireActivity())
+            .setChartType(chartTypeEnum)
+            .setBackgroundColor("#4b2b7f")
+            .setDataLabelsEnabled(false)
+            .setYAxisGridLineWidth(0f)
+            .setLegendEnabled(false)
+            .setTouchEventEnabled(true)
+            .setSeries(
                 AASeriesElement()
                     .name("Tokyo")
                     .data(arrayOf(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)),
+
+
                 )
-            )
-        view.aa_chart_view.aa_drawChartWithChartModel(aaChartModel)
-        return view
+            .build()
+
+        return aaChartModel
+    }
+
+    override fun chartViewDidFinishLoad(aaChartView: AAChartView) {
+    }
+
+    override fun chartViewMoveOverEventMessage(
+        aaChartView: AAChartView,
+        messageModel: AAMoveOverEventMessageModel
+    ) {
     }
 
 }
