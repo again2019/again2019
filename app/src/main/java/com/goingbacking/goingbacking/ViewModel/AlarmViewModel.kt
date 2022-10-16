9f696013718b189a892e6948e80a1396ffc649ff
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-16 13:16:03 +0900

    Refactor: AlarmViewModel mvvn 패턴화 완료

--- app/src/main/java/com/goingbacking/goingbacking/ViewModel/AlarmViewModel.kt
+++ app/src/main/java/com/goingbacking/goingbacking/ViewModel/AlarmViewModel.kt
@@ -16,11 +16,33 @@ class AlarmViewModel @Inject constructor(
     val addInitSaveTimeDay: LiveData<UiState<String>>
         get() = _addInitSaveTimeDay
 
+    private val _addInitSaveTimeMonth = MutableLiveData<UiState<String>>()
+    val addInitSaveTimeMonth: LiveData<UiState<String>>
+        get() = _addInitSaveTimeMonth
+
+    private val _addInitSaveTimeYear = MutableLiveData<UiState<String>>()
+    val addInitSaveTimeYear: LiveData<UiState<String>>
+        get() = _addInitSaveTimeYear
+
+
     fun addInitSaveTimeDayInfo() {
         _addInitSaveTimeDay.value = UiState.Loading
         alarmRepository.addInitSaveTimeDayInfo() {
             _addInitSaveTimeDay.value = it
         }
+    }
 
+    fun addInitSaveTimeMonthInfo() {
+        _addInitSaveTimeMonth.value = UiState.Loading
+        alarmRepository.addInitSaveTimeMonthInfo() {
+            _addInitSaveTimeMonth.value = it
+        }
+    }
+
+    fun addInitSaveTimeYearInfo() {
+        _addInitSaveTimeYear.value = UiState.Loading
+        alarmRepository.addInitSaveTimeYearInfo() {
+            _addInitSaveTimeYear.value = it
+        }
     }
 }
