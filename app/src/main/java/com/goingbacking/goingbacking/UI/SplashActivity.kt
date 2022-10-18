e09d10758c11290954422727b09cdb8d632ff5a7
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-18 23:50:51 +0900

    Refactor: 경로 재설정 및 viewbinding 형태로 변환

--- app/src/main/java/com/goingbacking/goingbacking/UI/SplashActivity.kt
+++ app/src/main/java/com/goingbacking/goingbacking/UI/SplashActivity.kt
@@ -1,11 +1,11 @@
-package com.goingbacking.goingbacking.Service
+package com.goingbacking.goingbacking.UI
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import com.goingbacking.goingbacking.LoginActivity
 import com.goingbacking.goingbacking.R
+import com.goingbacking.goingbacking.databinding.ActivitySplashBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,16 +14,19 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+    private val binding: ActivitySplashBinding by lazy {
+        ActivitySplashBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(binding.root)
 
         CoroutineScope(Dispatchers.IO).launch {
             delay(DURATION)
             var intent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(intent)
         }
-        
+
     }
 
     companion object {
