c1ea2b7716d9ba610e212366ba7dbf400992c279
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-19 15:17:57 +0900

    Refactor: Implementation FirstInputFragment

--- app/src/main/java/com/goingbacking/goingbacking/UI/Input/FirstInputFragment.kt
+++ app/src/main/java/com/goingbacking/goingbacking/UI/Input/FirstInputFragment.kt
@@ -1,22 +1,88 @@
 package com.goingbacking.goingbacking.UI.Input
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.goingbacking.goingbacking.Model.UserInfoDTO
 import com.goingbacking.goingbacking.R
+import com.goingbacking.goingbacking.UI.Base.BaseFragment
+import com.goingbacking.goingbacking.ViewModel.InputViewModel
+import com.goingbacking.goingbacking.databinding.FragmentFirstInputBinding
+import com.goingbacking.goingbacking.util.UiState
+import dagger.hilt.android.AndroidEntryPoint
 
-class FirstInputFragment : Fragment() {
+@AndroidEntryPoint
+class FirstInputFragment : BaseFragment<FragmentFirstInputBinding>() {
+    val viewModel: InputViewModel by viewModels()
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_input, container, false)
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentFirstInputBinding {
+       return FragmentFirstInputBinding.inflate(inflater, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        onClick()
+    }
+
+    private fun onClick() = with(binding) {
+        firstInputButton.setOnClickListener {
+            FirstInputObserver()
+            completeAction()
+        }
+        nickNameEdittext.setOnEditorActionListener { v, actionId, event ->
+            completeAction()
+            true
+        }
+    }
+
+    private fun completeAction() = with(binding) {
+        // 만약에 edittext가 비어있다면
+        if (TextUtils.isEmpty(nickNameEdittext.text)) {
+            Toast.makeText(requireActivity(), "닉네임을 입력하세요.", Toast.LENGTH_SHORT).show()
+        }
+        else if (nickNameEdittext.text.length >= 10) {
+            Toast.makeText(requireActivity(), "닉네임이 너무 길어요.", Toast.LENGTH_SHORT).show()
+        }
+        // 만약에 edittext가 비어있지 않다면
+        else {
+            viewModel.addFirstInput(
+                UserInfoDTO(
+                    nickNameEdittext.text.toString(),
+                    null,
+                    null,
+                    null,
+                )
+            )
+            findNavController().navigate(R.id.action_firstInputFragment_to_secondInputFragment)
+        }
+    }
+
+    private fun FirstInputObserver() {
+        viewModel.addFirstInput.observe(viewLifecycleOwner) {
+                state ->
+            when(state) {
+                is UiState.Failure -> {
+                    Toast.makeText(requireActivity(), "fail", Toast.LENGTH_SHORT).show()
+                }
+                is UiState.Success -> {
+                    Toast.makeText(requireActivity(), "success", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
 
 
 }
--- app/src/main/java/com/goingbacking/goingbacking/UI/Input/SecondInputFragment.kt
+++ app/src/main/java/com/goingbacking/goingbacking/UI/Input/SecondInputFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.goingbacking.goingbacking.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 
 class SecondInputFragment : Fragment() {
 
