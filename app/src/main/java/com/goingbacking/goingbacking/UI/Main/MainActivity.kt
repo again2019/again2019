ce68078927f52929a177c0bd7b72c63cb7dcd294
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-19 18:26:32 +0900

    Chore: 불필요한 import 삭제 (mainactivity)

--- app/src/main/java/com/goingbacking/goingbacking/UI/Main/MainActivity.kt
+++ app/src/main/java/com/goingbacking/goingbacking/UI/Main/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.goingbacking.goingbacking.UI.Main
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.goingbacking.goingbacking.R
 import com.goingbacking.goingbacking.UI.Base.BaseActivity
 import com.goingbacking.goingbacking.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 @AndroidEntryPoint
