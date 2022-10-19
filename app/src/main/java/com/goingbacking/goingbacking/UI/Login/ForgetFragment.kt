81fef34b4a4630b41cbd0348909d071fb64681ce
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-19 17:35:20 +0900

    Refactor: 비밀번호 찾기 서비스 구현

--- app/src/main/java/com/goingbacking/goingbacking/UI/Login/ForgetFragment.kt
+++ app/src/main/java/com/goingbacking/goingbacking/UI/Login/ForgetFragment.kt
@@ -15,31 +15,7 @@ import com.goingbacking.goingbacking.util.UiState
 import com.goingbacking.goingbacking.util.isValidEmail
 import dagger.hilt.android.AndroidEntryPoint
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ForgetFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-=======
->>>>>>> 6bc5987e0acc9a30f86433083f3a132d808e98c2
-class ForgetFragment : Fragment() {
-
-<<<<<<< HEAD
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-=======
 @AndroidEntryPoint
 class ForgetFragment : BaseFragment<FragmentForgetBinding>() {
     override fun getFragmentBinding(
@@ -74,34 +50,13 @@ class ForgetFragment : BaseFragment<FragmentForgetBinding>() {
                 is UiState.Failure -> {
                 }
                 is UiState.Success -> {
->>>>>>> Stashed changes
+
 
                 }
             }
         }
     }
 
-<<<<<<< Updated upstream
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ForgetFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ForgetFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-=======
     fun validation(): Boolean  = with(binding) {
         var isValid = true
 
@@ -116,17 +71,5 @@ class ForgetFragment : BaseFragment<FragmentForgetBinding>() {
         return isValid
     }
 
-
->>>>>>> Stashed changes
-=======
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forget, container, false)
-    }
-
->>>>>>> 6bc5987e0acc9a30f86433083f3a132d808e98c2
+    
 }
