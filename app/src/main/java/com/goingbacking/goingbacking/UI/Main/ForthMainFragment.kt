faf244ae6a67bd640e0bdfab14619b37cedfe23a
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-20 12:13:41 +0900

    Style: ForthMainFragment BaseFragment 적용

--- app/src/main/java/com/goingbacking/goingbacking/UI/Main/ForthMainFragment.kt
+++ app/src/main/java/com/goingbacking/goingbacking/UI/Main/ForthMainFragment.kt
@@ -2,30 +2,29 @@ package com.goingbacking.goingbacking.UI.Main
 
 import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.goingbacking.goingbacking.UI.Base.BaseFragment
 import com.goingbacking.goingbacking.databinding.FragmentForthMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ForthMainFragment : Fragment() {
+class ForthMainFragment : BaseFragment<FragmentForthMainBinding>() {
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentForthMainBinding {
+        return FragmentForthMainBinding.inflate(inflater, container, false)
 
-    lateinit var binding:FragmentForthMainBinding
+    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentForthMainBinding.inflate(layoutInflater)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val builder : AlertDialog.Builder? = AlertDialog.Builder(requireActivity())
+        val builder: AlertDialog.Builder? = AlertDialog.Builder(requireActivity())
         builder?.setTitle("안내")?.setMessage("서비스 준비중입니다.")
         builder?.show()
 
-        if(this::binding.isInitialized) {
-            return binding.root
-        } else {
-            binding = FragmentForthMainBinding.inflate(layoutInflater)
-            return binding.root
-        }
     }
 }
