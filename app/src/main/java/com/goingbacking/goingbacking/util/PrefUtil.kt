829c070c515329333a257e452c2110a9da596200
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-21 22:33:52 +0900

    Fix: Preference 자신의 계정만 저장할 수 있게 바꿈

--- app/src/main/java/com/goingbacking/goingbacking/util/PrefUtil.kt
+++ app/src/main/java/com/goingbacking/goingbacking/util/PrefUtil.kt
@@ -2,22 +2,22 @@ package com.goingbacking.goingbacking.util
 
 import android.content.Context
 import android.preference.PreferenceManager
+import com.google.firebase.auth.FirebaseAuth
 
 class PrefUtil {
 
     companion object {
 
-
         private const val SECONDS_REMAINING_ID = "seconds_remaining"
 
         fun getSecondsRemaining(context: Context): Long{
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return preferences.getLong(SECONDS_REMAINING_ID, 0)
+            return preferences.getLong(firebaseUid() + SECONDS_REMAINING_ID, 0)
         }
 
         fun setSecondsRemaining(seconds: Long, context: Context){
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putLong(SECONDS_REMAINING_ID, seconds)
+            editor.putLong(firebaseUid() + SECONDS_REMAINING_ID, seconds)
             editor.apply()
         }
 
@@ -26,13 +26,13 @@ class PrefUtil {
 
         fun setTodayTotalTime(time: Int, context: Context){
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putInt(ALARM_SET_Total_Today_TIME_ID, time)
+            editor.putInt(firebaseUid() + ALARM_SET_Total_Today_TIME_ID, time)
             editor.apply()
         }
 
         fun getTodayTotalTime(context:Context) : Int {
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return  preferences.getInt(ALARM_SET_Total_Today_TIME_ID, 0)
+            return  preferences.getInt(firebaseUid() + ALARM_SET_Total_Today_TIME_ID, 0)
 
         }
 
@@ -40,12 +40,12 @@ class PrefUtil {
 
         fun getEndTime(context: Context): Int{
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return  preferences.getInt(ALARM_SET_END_TIME_ID, 0)
+            return  preferences.getInt(firebaseUid() + ALARM_SET_END_TIME_ID, 0)
         }
 
         fun setEndTime(time: Int, context: Context){
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putInt(ALARM_SET_END_TIME_ID, time)
+            editor.putInt(firebaseUid() + ALARM_SET_END_TIME_ID, time)
             editor.apply()
         }
 
@@ -53,12 +53,12 @@ class PrefUtil {
 
         fun getStartTime(context: Context): Long{
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return  preferences.getLong(ALARM_SET_START_TIME_ID, 0L)
+            return  preferences.getLong(firebaseUid() + ALARM_SET_START_TIME_ID, 0L)
         }
 
         fun setStartTime(time: Long, context: Context){
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putLong(ALARM_SET_START_TIME_ID, time)
+            editor.putLong(firebaseUid() + ALARM_SET_START_TIME_ID, time)
             editor.apply()
         }
 
@@ -66,12 +66,12 @@ class PrefUtil {
 
         fun getTodayWhatToDo(context: Context): String? {
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return  preferences.getString(ALARM_SET_TODAY_WHAT_TO_DO_ID, "")
+            return  preferences.getString(firebaseUid() + ALARM_SET_TODAY_WHAT_TO_DO_ID, "")
         }
 
         fun setTodayWhatToDo(str: String, context: Context){
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putString(ALARM_SET_TODAY_WHAT_TO_DO_ID, str)
+            editor.putString(firebaseUid() + ALARM_SET_TODAY_WHAT_TO_DO_ID, str)
             editor.apply()
         }
 
@@ -79,13 +79,17 @@ class PrefUtil {
 
         fun getTodayWhatToDoTime(context: Context): String? {
             val preferences = PreferenceManager.getDefaultSharedPreferences(context)
-            return  preferences.getString(ALARM_SET_TODAY_WHAT_TO_DO_TIME_ID, "")
+            return  preferences.getString(firebaseUid() + ALARM_SET_TODAY_WHAT_TO_DO_TIME_ID, "")
         }
 
         fun setTodayWhatToDoTime(str: String, context: Context){
             val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
-            editor.putString(ALARM_SET_TODAY_WHAT_TO_DO_TIME_ID, str)
+            editor.putString(firebaseUid() + ALARM_SET_TODAY_WHAT_TO_DO_TIME_ID, str)
             editor.apply()
+        }
+
+        fun firebaseUid() : String {
+            return FirebaseAuth.getInstance().currentUser?.uid.toString()
         }
     }
 }
