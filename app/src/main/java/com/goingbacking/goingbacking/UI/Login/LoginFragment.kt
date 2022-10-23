bdbb3e0e56ef38de9a5d7c2ff6a472f20835c562
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-23 13:38:18 +0900

    Chore 불필요한 코드 삭제

--- app/src/main/java/com/goingbacking/goingbacking/UI/Login/LoginFragment.kt
+++ app/src/main/java/com/goingbacking/goingbacking/UI/Login/LoginFragment.kt
@@ -16,7 +16,6 @@ import com.goingbacking.goingbacking.UI.Input.InputActivity
 import com.goingbacking.goingbacking.UI.Main.MainActivity
 import com.goingbacking.goingbacking.ViewModel.LoginViewModel
 import com.goingbacking.goingbacking.databinding.FragmentLoginBinding
-import com.goingbacking.goingbacking.util.PrefUtil
 import com.goingbacking.goingbacking.util.UiState
 import com.goingbacking.goingbacking.util.isValidEmail
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -24,10 +23,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn.getClient
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
