100694657a2581c2b82cdc6c31c4d1414b2cdc3c
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-10-24 22:10:26 +0900

    Style: 로고 변경 및 Splash image 넣기

--- app/src/main/java/com/goingbacking/goingbacking/UI/Splash/SplashActivity.kt
+++ app/src/main/java/com/goingbacking/goingbacking/UI/Splash/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.goingbacking.goingbacking.UI.Splash
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.goingbacking.goingbacking.R
 import com.goingbacking.goingbacking.UI.Login.LoginActivity
 import com.goingbacking.goingbacking.UI.Base.BaseActivity
 import com.goingbacking.goingbacking.databinding.ActivitySplashBinding
@@ -11,21 +13,22 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-
+import android.os.Handler
 @AndroidEntryPoint
-class SplashActivity : BaseActivity<ActivitySplashBinding>({
-    ActivitySplashBinding.inflate(it)
-}) {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         CoroutineScope(Dispatchers.IO).launch {
             delay(DURATION)
             val intent = Intent(applicationContext, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
-            finish()
-        }
 
+
+        }
     }
 }
+
 
