554e648a2f1a32ce5e213a7f10f889021e272bba
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-11-02 11:48:05 +0900

    Chore: retrofit 주석 처리

--- app/src/main/java/com/goingbacking/goingbacking/FCM/FirebaseService.kt
+++ app/src/main/java/com/goingbacking/goingbacking/FCM/FirebaseService.kt
@@ -13,6 +13,7 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.IBinder
+import android.util.Log
 import androidx.annotation.RequiresApi
 import com.goingbacking.goingbacking.R
 import com.goingbacking.goingbacking.UI.Main.MainActivity
@@ -25,11 +26,16 @@ private const val CHANNEL_ID = "fcm_channel"
 
 class FirebaseService : FirebaseMessagingService() {
 
-    override fun onNewToken(newToken: String) {
-        super.onNewToken(newToken)
+    // 새로운 토큰으로 변경될 때
+    // 앱을 삭제하거나 다른 기기에서 실행시킬 때
+    override fun onNewToken(token: String) {
+        Log.d("experiment","new token1 ${token}")
+        super.onNewToken(token)
+        Log.d("experiment","new token2 ${token}")
 
     }
 
+    // 메시지를
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
--- app/src/main/java/com/goingbacking/goingbacking/FCM/NotificationAPI.kt
+++ app/src/main/java/com/goingbacking/goingbacking/FCM/NotificationAPI.kt
@@ -8,9 +8,12 @@ import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
+// REST API 명세에 맞는 Interface 선언
 interface NotificationAPI {
     @Headers("Authorization: key=$SERVER_KEY", "Content-Type:$CONTENT_TYPE")
     @POST("fcm/send")
+
+    // 서버 통신은 비동기 처리이기 때문에 코루틴 사용을 위해 suspend 처리
     suspend fun postNotification(
         @Body notification: PushNotification
     ) : Response<ResponseBody>
