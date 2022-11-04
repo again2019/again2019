6db2e4e9ca48c7ff2915472df2dd3d9d74284765
ansehun <63048392+ansehoon1999@users.noreply.github.com>
2022-11-04 18:09:58 +0900

    Style: 날짜 형식 변경

--- app/src/main/java/com/goingbacking/goingbacking/Repository/Forth/ForthRepository.kt
+++ app/src/main/java/com/goingbacking/goingbacking/Repository/Forth/ForthRepository.kt
@@ -11,6 +11,7 @@ import com.goingbacking.goingbacking.util.FBConstants.Companion.RANKMONTHINFO
 import com.goingbacking.goingbacking.util.FBConstants.Companion.RANKYEARINFO
 import com.goingbacking.goingbacking.util.PrefUtil
 import com.goingbacking.goingbacking.util.UiState
+import com.goingbacking.goingbacking.util.currentday
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
@@ -37,15 +38,10 @@ class ForthRepository (
 
 
     override fun getSaveTimeMonthInfo(result: (UiState<ArrayList<NewSaveTimeMonthDTO>>)-> Unit) {
-        var current = LocalDateTime.now()
-        current = current.minusDays(10)
-        val simpleDate1 = DateTimeFormatter.ofPattern("yyyy-MM")
-        val curYearMonth = current.format(simpleDate1)
-
         val arrayList = ArrayList<NewSaveTimeMonthDTO>()
 
-        firebaseFirestore.collection(RANKMONTHINFO).document(curYearMonth)
-            .collection(curYearMonth).orderBy(COUNT, Query.Direction.DESCENDING).get().addOnSuccessListener { documents ->
+        firebaseFirestore.collection(RANKMONTHINFO).document(currentday("yyyy-MM"))
+            .collection(currentday("yyyy-MM")).orderBy(COUNT, Query.Direction.DESCENDING).get().addOnSuccessListener { documents ->
                 for (document in documents) {
                     arrayList.add(document.toObject(NewSaveTimeMonthDTO::class.java))
                 }
@@ -57,14 +53,10 @@ class ForthRepository (
     }
 
     override fun getSaveTimeYearInfo(result: (UiState<ArrayList<NewSaveTimeYearDTO>>)-> Unit) {
-        val current = LocalDateTime.now()
-        val simpleDate = DateTimeFormatter.ofPattern("yyyy")
-        val curYear = current.format(simpleDate)
-
         val arrayList = ArrayList<NewSaveTimeYearDTO>()
 
-        firebaseFirestore.collection(RANKYEARINFO).document(curYear)
-            .collection(curYear).orderBy(COUNT, Query.Direction.DESCENDING).get().addOnSuccessListener { documents ->
+        firebaseFirestore.collection(RANKYEARINFO).document(currentday("yyyy"))
+            .collection(currentday("yyyy")).orderBy(COUNT, Query.Direction.DESCENDING).get().addOnSuccessListener { documents ->
                 for (document in documents) {
                     arrayList.add(document.toObject(NewSaveTimeYearDTO::class.java))
                 }
@@ -77,19 +69,10 @@ class ForthRepository (
 
     // 달별
     override fun likeButtonInfo(destinationUid :String, state :String, result: (UiState<String>) -> Unit) {
-        var current = LocalDateTime.now()
-        current = current.minusDays(10)
-        val simpleDate1 = DateTimeFormatter.ofPattern("yyyy-MM")
-        val simpleDate2 = DateTimeFormatter.ofPattern("yyyy")
-
-        val curYearMonth = current.format(simpleDate1)
-        val curYear = current.format(simpleDate2)
-
-
-        val tsDoc1 = firebaseFirestore.collection(RANKMONTHINFO).document(curYearMonth)
-            .collection(curYearMonth).document(destinationUid)
-        val tsDoc2 = firebaseFirestore.collection(RANKYEARINFO).document(curYear)
-            .collection(curYear).document(destinationUid)
+        val tsDoc1 = firebaseFirestore.collection(RANKMONTHINFO).document(currentday("yyyy-MM"))
+            .collection(currentday("yyyy-MM")).document(destinationUid)
+        val tsDoc2 = firebaseFirestore.collection(RANKYEARINFO).document(currentday("yyyy"))
+            .collection(currentday("yyyy")).document(destinationUid)
         if (state.equals("plus")) {
             tsDoc1.update("likes", FieldValue.arrayUnion(myUid))
             tsDoc2.update("likes", FieldValue.arrayUnion(myUid))
@@ -102,13 +85,8 @@ class ForthRepository (
     }
 
     override fun getCheerInfo(destinationUid: String, result: (UiState<List<String>>) -> Unit) {
-        var current = LocalDateTime.now()
-        current = current.minusDays(10)
-        val simpleDate = DateTimeFormatter.ofPattern("yyyy")
-        val curYear = current.format(simpleDate)
-
-        firebaseFirestore.collection(RANKYEARINFO).document(curYear)
-            .collection(curYear).document(destinationUid).get()
+        firebaseFirestore.collection(RANKYEARINFO).document(currentday("yyyy"))
+            .collection(currentday("yyyy")).document(destinationUid).get()
             .addOnSuccessListener { document ->
                 if (document != null) {
                     Log.d("experiment", document.toObject(NewSaveTimeYearDTO::class.java)!!.cheers.toString())
@@ -134,26 +112,15 @@ class ForthRepository (
     }
 
     override fun addCheerInfo(destinationUid: String, text: String, result: (UiState<List<String>>) -> Unit) {
-        var current = LocalDateTime.now()
-        current = current.minusDays(10)
-        val simpleDate = DateTimeFormatter.ofPattern("yyyy")
-        val curYear = current.format(simpleDate)
 
-        val tsDoc = firebaseFirestore.collection(RANKYEARINFO).document(curYear)
-            .collection(curYear).document(destinationUid)
+        val tsDoc = firebaseFirestore.collection(RANKYEARINFO).document(currentday("yyyy"))
+            .collection(currentday("yyyy")).document(destinationUid)
 
         CoroutineScope(Dispatchers.IO).launch {
             val destinationInfo = firebaseFirestore.collection(USERINFO).document(destinationUid).get().await().toObject(UserInfoDTO::class.java)
-            Log.d("experiment", destinationInfo.toString())
-
             val userInfo = firebaseFirestore.collection(USERINFO).document(myUid).get(cache).await().toObject(UserInfoDTO::class.java)
-            Log.d("experiment", userInfo.toString())
-
             val cheer = myUid + ":" + userInfo!!.userNickName + ":" + text
-//
-//            Log.d("experiment", cheer.toString())
             tsDoc.update("cheers", FieldValue.arrayUnion(cheer)).await()
-            Log.d("experiment", "ss")
 
             PushNotification(
                 NotificationData("title", text),
@@ -161,8 +128,8 @@ class ForthRepository (
             ).also {
                 FirebaseTokenManager.sendNotification(it)
             }
-            firebaseFirestore.collection(RANKYEARINFO).document(curYear)
-                .collection(curYear).document(destinationUid).get()
+            firebaseFirestore.collection(RANKYEARINFO).document(currentday("yyyy"))
+                .collection(currentday("yyyy")).document(destinationUid).get()
                 .addOnSuccessListener { document ->
                     if (document != null) {
                         Log.d("experiment", document.toObject(NewSaveTimeYearDTO::class.java)!!.cheers.toString())
@@ -193,14 +160,8 @@ class ForthRepository (
         text: String,
         result: (UiState<String>) -> Unit
     ) {
-        var current = LocalDateTime.now()
-        current = current.minusDays(10)
-        val simpleDate = DateTimeFormatter.ofPattern("yyyy")
-        val curYear = current.format(simpleDate)
-
-
-        val tsDoc = firebaseFirestore.collection(RANKYEARINFO).document(curYear)
-            .collection(curYear).document(destinationUid)
+        val tsDoc = firebaseFirestore.collection(RANKYEARINFO).document(currentday("yyyy"))
+            .collection(currentday("yyyy")).document(destinationUid)
         CoroutineScope(Dispatchers.IO).launch {
             tsDoc.update("cheers", FieldValue.arrayRemove(text)).await()
         }
